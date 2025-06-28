8a0fa17370a08c6ec64685265512740bb6007ef2
kannathasansdet <kannathasansdet@gmail.com>
2025-06-28 13:24:52 +0530

    Week1Task

---
+++ Task/Week1Task.sql
@@ -0,0 +1,47 @@
+—-------------------------------------------TASK 1 -------------------------------------------------------
+Create a database called LibraryDB
+
+Create tables for Books, Authors, and Borrowers.
+
+Authors 
+    AuthorID 
+    AuthorName 
+
+Books 
+    BookID 
+    BookTitle 
+    AuthorID 
+
+Borrowers 
+    BorrowerID 
+    BorrowerName 
+    BorrowedBookID 
+
+Insert 3 records for each tables (With column )
+
+Insert 3 records for each tables (Without column)
+
+Insert 3 records for each table  (Multiple insertion with column)
+
+Total 9 records for each table
+
+Add a new column price to the Books table
+
+Again try to add a value with newly inserted column 
+
+—----------------------------------------------------TASK 2------------------------------------------------------------
+
+Create a database called Employee Management System
+
+Create a table named "Employees" with columns for ID, Name, Age, and Salar (DECIMAL).
+
+Add a new column "Department" to the "Employees" table.
+
+Change the data type of the "Salary" column to INTEGER.
+
+Remove the "Department" column from the "Employees" table.
+
+Insert a new employee record into the "Employees" table.
+
+Insert multiple employee records into the "Employees" table.
+
